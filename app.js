0f5d126c72cc0b6a12aaa01539f9eeaa96484521
Marco Siguencia <m08251983@gmail.com>
2017-07-29 21:20:41 -0400

    Hello World
    I've written just enough to get a "Hello World".
    
    Let me get a "Hello World"!
    
    app.js:
            - Hooking up our router to our app. The routes the router will be using
              are defined in the index.js file. We do this by importing the router
              as routes, and allowing our app to use the router for all paths with
              the root "/".
    
    index.js:
            - An index of routes. Path/routes are associated with their handlers
              here. In the future we'll use dependency injection to decouple the
              handler definitions from the route definition. This allows us to
              import our handlers and reference them instead of hard-coding
              them into the index.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var express = require('express');
+const express = require('express');
+const routes = require('./routes/index');
 
 //create our Express app
 const app = express();
 
+app.use('/', routes);
 module.exports = app;
