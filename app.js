c0da5e9d29cd3d5304fd1c38d41ead15aa1a0f77
Marco Siguencia <m08251983@gmail.com>
2017-07-31 01:21:23 -0400

    Setting Up the View Engine
    app.js:
            - Hooking up the view engine to the app and setting the directory for views
    
    routes/index.js:
            - Changing the response behavior from sending a string to
              sending/rendering an html file.
    
    views/layout.pug:
            - a simple view file that is rendered by the index handler. Previously,
              the handler would send a string as a response. Now it sends a view
              file. All it does is describe an html file that displays "Hey! It
              works!!"

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes/index');
 
 //create our Express app
 const app = express();
+
+//setting where we keep our pug files (templates) and the view engine to pug
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use('/', routes);
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (request, response) => {
-    response.send('Hey! This works!!!');
+    //response.send('Hey! This works!!!');
+    response.render('layout');
 });
 
 module.exports = router;
