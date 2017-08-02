aa0b17fe0fce86c23c27ff4af510269ffd5f2987
Marco Siguencia <m08251983@gmail.com>
2017-08-02 15:53:26 -0400

    Connecting to MongoDB DB
    start.js:
            - Using mongoose to connect to our online MongoDB instance
    
            - The app doesn't make use of the db just yet. All we do in the start
              file is connect to the db and set mongoose to use ES6 Promises.

--- start.js
+++ start.js
@@ -2,8 +2,17 @@
 //import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) => {
+    console.error(`DB: ${err.message}`);
+});
+
 //Starting our app!
 const app = require('./app');
+//app is an instance of express
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
     console.log(`Express running on PORT ${server.address().port}`);
