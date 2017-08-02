ffb25056cd24cae490ee5b4288d3e1794080073d
Marco Siguencia <m08251983@gmail.com>
2017-08-02 00:20:04 -0400

    Adding bodyParser dependency on our app
    We make our app use the json portion of the bodyParser module to take raw
    requests and turn them into properties found in req.body.
    
    app.js:
            - Adding bodyParser to our app so that raw requests can be turned into
              properties on req.body. It'll make our lives easier when we submit
              forms in our application.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
+const bodyParser = require('body-parser');
 
 //create our Express app
 const app = express();
@@ -9,5 +10,10 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//Takes raw requests and turns them into properties found in req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/', routes);
+
 module.exports = app;
